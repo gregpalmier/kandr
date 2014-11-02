1e49e989cca1575bf2e04dab69ad5de6940b492a
Greg Palmier <gregpalmier@gmail.com>
2014-11-02 16:13:37 -0600

    1-8

---
+++ 1/8.c
@@ -0,0 +1,15 @@
+/* Write a program to count blanks, tabs, and newlines */
+#include <stdio.h>
+main(){
+  int c;
+  int b = 0, t = 0, nl = 0;
+  while ((c = getchar()) != EOF ){
+    if (c == ' ')
+      ++b;
+    if (c == '\t')
+      ++t;
+    if (c == '\n')
+      ++nl;
+    printf("%d %d %d\n", b, t, nl);
+  }
+}
