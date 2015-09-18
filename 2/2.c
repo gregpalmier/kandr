c3f75bfd3bab353c08831e5622fd4b5cb9c4050d
Greg Palmier <gregpalmier@gmail.com>
2015-09-17 20:25:04 -0500

    2

---
+++ 2/2.c
@@ -0,0 +1,22 @@
+/* Exercise 2-2. Write a loop equivalent to the for loop above without using && or ||.
+   for (i=0; i < lim-1 && (c=getchar()) != ’\n’ && c != EOF; ++i)
+     s[i] = c;
+*/
+#include<stdio.h>
+#define MAXLINE 80
+int main(void){
+int len = MAXLINE;
+char s[len];
+int c;
+int i = 0;
+while (i < (len - 1)){
+  c = getchar();
+  if (c == EOF)
+    break;
+  else if (c == '\n')
+    break;
+  s[i] = c;
+  ++i; }
+  s[i] = '\0';   /* terminate the string */
+  printf("c: %s", s);
+}
